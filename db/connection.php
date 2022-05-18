34ca03dbdd1e59358f3097a9164511d8f73f7594
Hju Kneyck Flores <hjucode@gmail.com>
2022-05-18 22:37:10 +0800

    refactor: update db connection logic

--- db/connection.php
+++ db/connection.php
@@ -1,11 +1,26 @@
 <?php
 function get_db_connection() {
-    $conn = new mysqli("localhost", "root", "", "dubai_pasabuy");
+    $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $cleardb_server = $cleardb_url["host"];
+    $cleardb_username = $cleardb_url["user"];
+    $cleardb_password = $cleardb_url["pass"];
+    $cleardb_db = substr($cleardb_url["path"],1);
 
-    if ($err = $conn->connect_error) {
-        die($err);
+    // During deployment, uncomment the line of codes below comment the ones above 
+    // Replace the values as needed
+
+    // $cleardb_server = "127.0.0.1";
+    // $cleardb_username = "username";
+    // $cleardb_password = "password";
+    // $cleardb_db = "database_table_name";
+
+    $conn = new mysqli($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
+    if ($conn->connect_error) {
+        echo $conn->connect_error;
+        return null;
     } else {
-        return $conn; 
+        return $conn;
     }
 }
 
