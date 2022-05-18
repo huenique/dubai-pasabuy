9bacbea4336ead5b59982c644efd6c3845cf3e21
Hju Kneyck Flores <hjucode@gmail.com>
2022-05-18 22:34:57 +0800

    refactor: update sql tables

--- db/sql/dubai_pasabuy.sql
+++ db/sql/dubai_pasabuy.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 18, 2022 at 09:42 AM
+-- Generation Time: May 18, 2022 at 04:34 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
@@ -24,35 +24,13 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `products`
---
-
-CREATE TABLE `products` (
-  `id` varchar(7) NOT NULL,
-  `name` varchar(128) NOT NULL,
-  `cost_aed` varchar(256) DEFAULT NULL,
-  `cost_php` varchar(256) DEFAULT NULL,
-  `access` tinytext DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `products`
---
-
-INSERT INTO `products` (`id`, `name`, `cost_aed`, `cost_php`, `access`) VALUES
-('175c206', 'Perfume', NULL, NULL, 'onhand'),
-('30da5bd', 'Cologne', NULL, NULL, 'next');
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `customers`
 --
 
 CREATE TABLE `customers` (
   `id` int(11) NOT NULL,
-  `username` varchar(320) NOT NULL,
-  `password` varchar(256) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
   `cart` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -61,22 +39,65 @@ CREATE TABLE `customers` (
 --
 
 INSERT INTO `customers` (`id`, `username`, `password`, `cart`) VALUES
-(20, 'test@gg.com', '123', NULL);
+(20, 'test@gg.com', '123', '{\"30\":\"5\",\"175\":\"3\"}');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `item` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `pasabuy_fee` decimal(15,2) NOT NULL,
+  `total_value` decimal(15,2) NOT NULL,
+  `total_collection` decimal(15,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `name` varchar(128) NOT NULL,
+  `cost_aed` decimal(15,2) DEFAULT NULL,
+  `cost_php` decimal(15,2) DEFAULT NULL,
+  `access` tinytext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `cost_aed`, `cost_php`, `access`) VALUES
+(30, 'Cologne', NULL, NULL, 'next'),
+(175, 'Perfume', NULL, NULL, 'onhand');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `products`
---
-ALTER TABLE `products`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Indexes for table `customers`
 --
 ALTER TABLE `customers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -88,6 +109,12 @@ ALTER TABLE `customers`
 --
 ALTER TABLE `customers`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
